56dd116855c7b34b106bde63879d659958632598
Ali Batu Sarıca <alibsrc@outlook.com>
2025-03-26 00:16:53 +0300

    Added KeyHandler.java
    Added KeyHandler class which manages player movement.

---
+++ KeyHandler.java
@@ -0,0 +1,59 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener
+{
+    public boolean upPressed;
+    public boolean downPressed;
+    public boolean rightPressed;
+    public boolean leftPressed;
+
+    public void keyTyped(KeyEvent e)
+    {
+        //TODO
+    }
+
+    public void keyPressed(KeyEvent e)
+    {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W)
+        {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_S)
+        {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_A)
+        {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_D)
+        {
+            rightPressed = true;
+        }
+    }
+    
+    public void keyReleased(KeyEvent e)
+    {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W)
+        {
+            upPressed = false;
+        }
+        if (code == KeyEvent.VK_S)
+        {
+            downPressed = false;
+        }
+        if (code == KeyEvent.VK_A)
+        {
+            leftPressed = false;
+        }
+        if (code == KeyEvent.VK_D)
+        {
+            rightPressed = false;
+        }
+    }
+}   
