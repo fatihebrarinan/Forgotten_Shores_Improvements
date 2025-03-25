1a937ff42f1325057a90e9e490aa0b2d63b5d100
Ali Batu Sarıca <alibsrc@outlook.com>
2025-03-26 00:16:02 +0300

    Implemented basic player movement mechanics
    Added the basic structures of player movement and the player objects displayal on the main frame.

--- Panel.java
+++ Panel.java
@@ -1,28 +1,41 @@
-
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
-public class Panel extends JPanel implements Runnable {
+public class Panel extends JPanel implements Runnable 
+{
 
-    final int ORIGINAL_TILE_SIZE = 16;
+    final int originalTileSize = 16;
     final int scale = 3;
 
-    final int FINAL_TILE_SIZE = scale * ORIGINAL_TILE_SIZE;
+    final int tileSize = scale * originalTileSize;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
-    final int screenWidth = FINAL_TILE_SIZE * maxScreenCol;
-    final int screenHeight = FINAL_TILE_SIZE * maxScreenRow;
-
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
-    public Panel() {
+    int fps = 60;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+
+    public Panel() 
+    {
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
-    public void startGameThread() {
+    public void startGameThread() 
+    {
         
         gameThread = new Thread(this);
         gameThread.start();
@@ -30,8 +43,101 @@ public class Panel extends JPanel implements Runnable {
     }
 
     @Override
-    public void run() {
+    /*public void run()
+    {
+        double drawInterval = 1000000000 / fps;
+        double nextDrawTime = System.nanoTime() + drawInterval;    
 
+        while(gameThread != null)
+        {
+            // Updates character position
+            update();
+
+            // Draws the screen with the updated information
+            repaint();
+
+            try
+            {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000;
+
+                if (remainingTime < 0)
+                {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+                nextDrawTime += drawInterval;
+            }
+            catch (InterruptedException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+    }*/
+
+    public void run()
+    {
+        double drawInterval = 1000000000 / fps;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null)
+        {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) 
+            {
+                update();
+                repaint();
+                drawCount++;
+                delta--;
+            }
+            if (timer >= 1000000000)
+            {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update()
+    {
+        if (keyH.upPressed == true)
+        {
+            playerY -= playerSpeed; 
+        }
+        else if (keyH.downPressed == true)
+        {
+            playerY += playerSpeed; 
+        }
+        else if (keyH.leftPressed == true)
+        {
+            playerX -= playerSpeed; 
+        }
+        else if (keyH.rightPressed == true)
+        {
+            playerX += playerSpeed; 
+        }
     }
     
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.WHITE);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
+    }
 }
