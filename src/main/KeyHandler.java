0d5c3507fb4c3205a0461a6a6ee333a6ed8c25f3
Ali Batu Sarıca <alibsrc@outlook.com>
2025-03-29 15:36:44 +0300

    Added if statements for game state
    Added pause and unpause key bind "esc" for pausing the game. Also modified the constructor.

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -5,11 +5,18 @@ import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener
 {
+    GamePanel gp;
+    
     public boolean upPressed;
     public boolean downPressed;
     public boolean rightPressed;
     public boolean leftPressed;
     public boolean fPressed;
+
+    public KeyHandler(GamePanel gp)
+    {
+        this.gp = gp;
+    }
 
     public void keyTyped(KeyEvent e)
     {
@@ -39,6 +46,17 @@ public class KeyHandler implements KeyListener
         if (code == KeyEvent.VK_F)
         {
             fPressed = true;
+        }
+        if (code == KeyEvent.VK_ESCAPE)
+        {
+            if (gp.gameState == gp.playState)
+            {
+                gp.gameState = gp.pauseState;
+            }
+            else if (gp.gameState == gp.pauseState)
+            {
+                gp.gameState = gp.playState;
+            }
         }
     }
     
